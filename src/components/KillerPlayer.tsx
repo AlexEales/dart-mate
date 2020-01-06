75b7afdb3a2727ffa0fde98e605259099a8d6dea
Alex Eales <alexeales@hotmail.co.uk>
2020-01-06 11:40:29 +0000

    Added reviving players in killer game

--- src/components/KillerPlayer.tsx
+++ src/components/KillerPlayer.tsx
@@ -18,6 +18,15 @@ export default class KillerPlayer extends Component<Props, State> {
         };
     }
 
+    revive = () => {
+        this.setState(prev => {
+            return {
+                dead: false,
+                lives: 1
+            };
+        });
+    }
+
     add = () => {
         this.setState(prev => {
             return { lives: prev.lives + 1 > 5 ? 5 : prev.lives + 1 };
@@ -38,7 +47,11 @@ export default class KillerPlayer extends Component<Props, State> {
         return (
             <div class={"killer-player" + (dead ? " killer-player--dead" : "")}>
                 <h2 class="killer-player__name">{name}</h2>
-                <h3 class={'killer-player__lives-' + lives}></h3>
+                {
+                    dead
+                    ? <button class="killer-player__revive" onClick={this.revive}>Revive</button>
+                    : <h3 class={'killer-player__lives-' + lives}></h3>
+                }
                 <div class="killer-player__controls">
                     <button class="killer-player__add" onClick={this.add} disabled={dead}>+</button>
                     <button class="killer-player__remove" onClick={this.subtract} disabled={dead}>−</button>
