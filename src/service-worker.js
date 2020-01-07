ca0cb1caf42f52106feff1239c1fbd2ba1538428
Alex Eales <alexeales@hotmail.co.uk>
2020-01-07 12:13:59 +0000

    Added installability to PWA

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,6 +14,24 @@ if (workbox) {
             cacheName: 'css-cache',
         })
     );
+    // Cache images
+    workbox.routing.registerRoute(
+        // Cache image files.
+        /\.(?:png|jpg|jpeg|svg|gif)$/,
+        // Use the cache if it's available.
+        new workbox.strategies.CacheFirst({
+            // Use a custom cache name.
+            cacheName: 'image-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                // Cache only 20 images.
+                maxEntries: 20,
+                // Cache for a maximum of a week.
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+                })
+            ],
+        })
+    );
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
